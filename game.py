fbd870c1964c3ee69140f6580fdbdc19177c350b
ary4n99 <ary4n99@gmail.com>
2020-11-25 23:19:49 +0000

    Adds bosskey and leaderboard changes

--- game.py
+++ game.py
@@ -7,18 +7,19 @@ def quitgame():
         window.destroy()
 
 def windowconfig():
-    global window, canvas
+    global window, canvas, workphoto, workphotolabel
     window = Tk()
-    window.geometry("1920x1080")
-    window.attributes("-fullscreen", True)
-    window.bind('<Escape>', lambda x: quitgame())
-    window.bind("p", lambda x: pausegame("Paused"))
-    window.bind("<Key>", moveplayer)
     canvas = Canvas(window, width=1920, height=1080)
     canvas.pack()
+    window.geometry("1920x1080")
+    window.attributes("-fullscreen", True)
+    
+    workphoto = PhotoImage(file="ss.gif")
+    workphotolabel = Label(image = workphoto)
+    workphotolabel.image = workphoto
 
 def level_static():
-    global obstacle
+    global obstacle, startarea, endarea
     bordercolour = "black"
     canvas.create_rectangle(0, 0, 1920, 150, fill=bordercolour, width=0)
     canvas.create_rectangle(0, 930, 1920, 1080, fill=bordercolour, width=0)
@@ -61,7 +62,7 @@ def moveplayer(event):
     elif event.char == "s": canvas.move(player, 0, move_factor)
 
 def welcomepage():
-    global startbutton, leaderbutton, welcometext, esctext, titletext, pausetext
+    global startbutton, leaderbutton, welcometext, esctext, titletext, pausetext, cheattext
     canvas.configure(bg="black")
     
     startbutton = Button(canvas, text="Start", font=("Helvetica", 20), command=startgame)
@@ -73,7 +74,8 @@ def welcomepage():
     titletext = canvas.create_text(960, 300, text="The Impossible Game", font=("Helvetica", 30), fill="white")
     welcometext = canvas.create_text(960, 450, text="Welcome!", font=("Helvetica", 80), fill="white")
     esctext = canvas.create_text(960, 800, text="Press esc to quit at anytime", font=("Helvetica", 10), fill="white")
-    pausetext = canvas.create_text(960, 900, text="Press p to toggle pause at anytime", font=("Helvetica", 10), fill="white")
+    pausetext = canvas.create_text(960, 850, text="Press p to toggle pause at anytime", font=("Helvetica", 10), fill="white")
+    cheattext = canvas.create_text(960, 950, text="Press x to toggle work mode (boss key) anytime", font=("Helvetica", 10), fill="white")
 
 def startgame():
     screenclear()
@@ -85,17 +87,18 @@ def startgame():
 def screenclear():
     leaderbutton.destroy()
     startbutton.destroy()
-    canvas.delete(welcometext, titletext, esctext, pausetext)
+    canvas.delete(welcometext, titletext, esctext, pausetext, cheattext)
 
 def leaderboard():
     global gohomebutton, leadertext
     with open("leaderboard.txt") as file:
         leaderboard = [line.strip() for line in file]
-    
     screenclear()
     leadertext = []
     for i in range(len(leaderboard)):
-        leadertext.append(canvas.create_text(960, i*100+100, text=leaderboard[i], font=("Helvetica", 30), fill="white"))
+        leadersplit = leaderboard[i].split(",")
+        leadertext.append(canvas.create_text(660, i*100+100, text = str(i+1) + ". "+ leadersplit[0], font=("Helvetica", 30), fill="white"))
+        leadertext.append(canvas.create_text(1260, i*100+100, text = "Score: "+ leadersplit[1], font=("Helvetica", 30), fill="white"))
     
     gohomebutton = Button(canvas, text="Go home", font=("Helvetica", 20), command=deleteleaderpage)
     gohomebutton.place(x=960, y=1000, anchor=CENTER)
@@ -107,41 +110,68 @@ def deleteleaderpage():
     welcomepage()
     
 def pausegame(input):
-    global pause, pausetext, pausepopup, initialrun
+    global pause, pausetext, pausepopup, initialrun, currentlevel
     if initialrun != True:
         pause = not pause
         if pause == True:
             pausetext = canvas.create_text(960, 450, text=input, font=("Helvetica", 80), fill="Black")
-            pausepopup = canvas.create_rectangle(0, 0, 1920,1080, fill="black", stipple="gray75")
+            pausepopup = canvas.create_rectangle(0, 0, 1920,1080, fill="black", stipple="gray25")
             window.bind("<Key>", collisiondetection)
         else:
             canvas.delete(pausetext, pausepopup)
             window.bind("<Key>", moveplayer)
-            restartgame()
+            if currentlevel == 1:
+                level_one()
 
 def restartgame():
-    global currentlevel
-    if currentlevel == 1:
-        level_one()
+     screenclear()
+     gameoverbutton.destroy()
+     canvas.delete(pausepopup, player, startarea, endarea)
+     for i in range(len(obstacle)):
+         canvas.delete(obstacle[i])
+     initialize()
+     welcomepage()
 
 def collisiondetection():
+    global gameoverbutton
     collision[0] = canvas.find_overlapping(obstaclecoords[0][0], obstaclecoords[0][1], obstaclecoords[0][2], obstaclecoords[0][3])
     for i in range(len(collision)):
-        if collision[i][0] == 5:
+        if len(collision[i]) == 2:
             pausegame("Game Over!")
+            gameoverbutton = Button(canvas, text="Restart", font=("Helvetica", 20), command=restartgame)
+            gameoverbutton.place(x=960, y=600, anchor=CENTER)
         else:
             window.after(2, collisiondetection)    
 
+def bosskey():
+    global bossmode, workphotolabel, workphoto
+    bossmode = not bossmode
+    if bossmode == True:
+        workphotolabel.place(x=-2, y=-2)
+    else:
+        workphotolabel.destroy()
+        workphotolabel = Label(image = workphoto)
+        workphotolabel.image = workphoto
 
-currentlevel = 1
-obstacle = []
-obstacledirection = []
-obstaclespeed = []
-obstaclecoords = [()]
-collision= [()]
-initialrun = True
-pause = False
+
+def initialize():
+    global currentlevel, obstacle, obstacledirection, obstaclespeed, obstaclecoords, collision, initialrun, pause, bossmode
+    currentlevel = 1
+    obstacle = []
+    obstacledirection = []
+    obstaclespeed = []
+    obstaclecoords = [()]
+    collision= [()]
+    initialrun = True
+    pause = False
+    bossmode = False
+    window.bind('<Escape>', lambda x: quitgame())
+    window.bind("p", lambda x: pausegame("Paused"))
+    window.bind("x", lambda x: bosskey())
+    window.bind("<Key>", moveplayer)
 
 windowconfig()
+initialize()
 welcomepage()
+
 window.mainloop()
